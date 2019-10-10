2867fe5c1f6930c2aec267609d76f68ca9a0bf34
Anton <antmakov@gmail.com>
2019-10-10 08:24:34 -0800

    updated rate app dialog instead of feedback

--- app/src/main/java/app/globe/com/weatherglobe/dialogs/AboutDialog.kt
+++ app/src/main/java/app/globe/com/weatherglobe/dialogs/AboutDialog.kt
@@ -1,6 +1,7 @@
 package app.globe.com.weatherglobe.dialogs
 
 import android.app.Dialog
+import android.content.ActivityNotFoundException
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
@@ -8,27 +9,17 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.view.WindowManager
-import android.widget.TextView
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
-import app.globe.com.weatherglobe.R
 import app.globe.com.weatherglobe.viewmodels.AboutViewModel
 import app.globe.com.weatherglobe.viewmodels.ViewModelFactory
-import butterknife.BindView
-import butterknife.ButterKnife
-import butterknife.Unbinder
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
-import com.google.android.material.button.MaterialButton
+import kotlinx.android.synthetic.main.about_dialog.*
 import javax.inject.Inject
 
 
 open class AboutDialog private constructor() : BottomSheetDialogFragment() {
 
-    @BindView(R.id.app_version)
-    lateinit var appVersion : TextView
-
-    @BindView(R.id.contact_me)
-    lateinit var contactMeBtn : MaterialButton
 
     @set:Inject
     internal var viewModelFactory: ViewModelFactory? = null
@@ -36,32 +27,29 @@ open class AboutDialog private constructor() : BottomSheetDialogFragment() {
 
     private var viewModel: AboutViewModel? = null
 
-    companion object{
-        fun newInstance() : AboutDialog{
+    companion object {
+        fun newInstance(): AboutDialog {
             return AboutDialog()
         }
     }
 
-    private var unbinder : Unbinder?=null
 
-    override fun getTheme(): Int = R.style.BaseBottomSheetDialog
+    override fun getTheme(): Int = app.globe.com.weatherglobe.R.style.BaseBottomSheetDialog
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val view = inflater.inflate(R.layout.about_dialog, container, false)
-        unbinder = ButterKnife.bind(this, view)
-        return view
+        return inflater.inflate(app.globe.com.weatherglobe.R.layout.about_dialog, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         viewModel = ViewModelProviders.of(this, viewModelFactory).get(AboutViewModel::class.java)
 
-        contactMeBtn.setOnClickListener {
+        rate_this_app.setOnClickListener {
 
-            composeEmail()
+            rateThisApp()
 
         }
 
@@ -75,28 +63,40 @@ open class AboutDialog private constructor() : BottomSheetDialogFragment() {
         return dialog
     }
 
-    private fun composeEmail() {
-        val intent = Intent(Intent.ACTION_SENDTO)
-        intent.data = Uri.parse("mailto:casterapps@gmail.com") // only email apps should handle this
-        intent.putExtra(Intent.EXTRA_SUBJECT, getString(R.string.app_feedback_subject))
-        startActivity(Intent.createChooser(intent, getString(R.string.send_feedback)))
+
+    private fun rateThisApp() {
+        val uri = Uri.parse("market://details?id=" + context!!.packageName)
+        val goToMarket = Intent(Intent.ACTION_VIEW, uri)
+        // To count with Play market backstack, After pressing back button,
+        // to taken back to our application, we need to add following flags to intent.
+        goToMarket.addFlags(
+            Intent.FLAG_ACTIVITY_NO_HISTORY or
+                    Intent.FLAG_ACTIVITY_NEW_DOCUMENT or
+                    Intent.FLAG_ACTIVITY_MULTIPLE_TASK
+        )
+        try {
+            startActivity(goToMarket)
+        } catch (e: ActivityNotFoundException) {
+            startActivity(
+                Intent(
+                    Intent.ACTION_VIEW,
+                    Uri.parse("http://play.google.com/store/apps/details?id=" + context!!.packageName)
+                )
+            )
+        }
+
     }
 
     private fun observableViewModel() {
 
         viewModel!!.getVersion().observe(this, Observer<String> { version ->
-           appVersion.text = version
+            app_version.text = version
         })
     }
 
     override fun onResume() {
         super.onResume()
         viewModel!!.loadAppVersion(context!!)
-    }
-
-    override fun onDestroyView() {
-        super.onDestroyView()
-        unbinder!!.unbind()
     }
 
 }
