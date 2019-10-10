770ca88910085af746062c803dde918742fa1ba2
Anton <antmakov@gmail.com>
2019-10-10 08:25:46 -0800

    updated shared prefs functionalities from settings

--- app/src/main/java/app/globe/com/weatherglobe/preference/SharedPrefs.kt
+++ app/src/main/java/app/globe/com/weatherglobe/preference/SharedPrefs.kt
@@ -1,83 +1,69 @@
 package app.globe.com.weatherglobe.preference
 
 import android.app.Application
+import android.content.Context
 import android.content.SharedPreferences
 import android.preference.PreferenceManager
 import app.globe.com.weatherglobe.R
+import app.globe.com.weatherglobe.utils.PREF_FILE_NAME
+import java.util.*
 import javax.inject.Inject
 
 
 class SharedPrefs @Inject constructor(val application : Application){
 
-    private var prefsSettings : SharedPreferences?=null
-    private var liveSharedPreferences : LiveSharedPreferences?=null
+    private var mPrefs: SharedPreferences = application.getSharedPreferences(PREF_FILE_NAME, Context.MODE_PRIVATE)
 
+    private var prefsSettings : SharedPreferences = PreferenceManager.getDefaultSharedPreferences(application)
+    private var liveSharedPreferences : LiveSharedPreferences = LiveSharedPreferences(prefsSettings)
 
-    init {
-        prefsSettings = PreferenceManager.getDefaultSharedPreferences(application)
-        liveSharedPreferences = LiveSharedPreferences(prefsSettings!!)
+    //----------------------------Shared Preferences--------------------------------------
+
+    fun setLastTimeAppWasOpened(time : Long) {
+        mPrefs.edit().putLong(application.getString(R.string.last_time_app_was_opened_key), time).apply()
     }
 
+    fun getLastTimeAppWasOpened(): Long {
+        val lastTimeAppWasOpened = Calendar.getInstance().timeInMillis
+        return mPrefs.getLong(application.getString(R.string.last_time_app_was_opened_key), lastTimeAppWasOpened)
+    }
+
+    //---------------------------SETTINGS-------------------------------------------------
     fun getTempUnitsLivePreference() : LivePreference<String>
     {
-        return liveSharedPreferences!!.getString(application.getString(R.string.temperature_unit_pref_key), "0")
+        return liveSharedPreferences.getString(application.getString(R.string.temperature_unit_pref_key), "0")
     }
 
     fun getDistanceUnitsLivePreference() : LivePreference<String>
     {
-        return liveSharedPreferences!!.getString(application.getString(R.string.distance_unit_pref_key), "0")
+        return liveSharedPreferences.getString(application.getString(R.string.distance_unit_pref_key), "0")
+    }
+
+    fun getDateFormatLivePreference() : LivePreference<String>
+    {
+        return liveSharedPreferences.getString(application.getString(R.string.date_format_pref_key),"0")
+    }
+
+    fun getTimeFormatLivePreference() : LivePreference<String>
+    {
+        return liveSharedPreferences.getString(application.getString(R.string.time_format_pref_key),"0")
     }
 
     fun getDistanceUnit() : Int
     {
-        return prefsSettings!!.getString(application.getString(R.string.distance_unit_pref_key),"0")!!.toInt()
+        return prefsSettings.getString(application.getString(R.string.distance_unit_pref_key),"0")!!.toInt()
     }
 
     fun getTempUnit(): Int {
-        return prefsSettings!!.getString(application.getString(R.string.temperature_unit_pref_key),"0")!!.toInt()
+        return prefsSettings.getString(application.getString(R.string.temperature_unit_pref_key),"0")!!.toInt()
     }
 
-    fun setTempUnit(unit : String)
-    {
-        prefsSettings!!.edit().putString(application.getString(R.string.temperature_unit_pref_key),
-            unit
-        ).apply()
+    fun getDateFormat() : Int{
+        return prefsSettings.getString(application.getString(R.string.date_format_pref_key),"0")!!.toInt()
     }
 
-    fun setDistanceUnit(unit : String)
-    {
-        prefsSettings!!.edit().putString(application.getString(R.string.distance_unit_pref_key),unit).apply()
+    fun getTimeFormat() : Int{
+        return prefsSettings.getString(application.getString(R.string.time_format_pref_key),"0")!!.toInt()
     }
-
-
-//    override fun setDistanceUnit(unit: Int) {
-//        distancePrefs.value = unit
-//    }
-//
-//    override fun setTempUnit(unit: Int) {
-//        temperaturePrefs.value = unit
-//    }
-
-
-//    override fun getDistanceUnit(): Int {
-//        return  prefsSettings.getString(application.getString(R.string.distance_unit_pref_key),"0")!!.toInt()
-//    }
-//
-//
-//    override fun getTempUnit(): Int {
-//        return prefsSettings.getString(application.getString(R.string.temperature_unit_pref_key),"0")!!.toInt()
-//    }
-//
-//    fun getTempPrefsLiveData() : LiveData<Int>
-//    {
-//        return temperaturePrefs
-//    }
-//
-//    fun getDistancePrefsLiveData() : LiveData<Int>
-//    {
-//        return distancePrefs
-//    }
-
-
 
 }
